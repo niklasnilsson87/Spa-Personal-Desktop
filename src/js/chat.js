e36f152e1531b396a32a40c3a748630324a02233
Niklas Nilsson <niklas.nilsson1987@gmail.com>
2018-12-26 22:07:22 +0100

    adding chat to PWD

--- src/js/chat.js
+++ src/js/chat.js
@@ -4,17 +4,16 @@ import { mainCSS } from './mainCSS.js'
 class Chat extends window.HTMLElement {
   constructor () {
     super()
+    this.socket = null
     this.apiKey = 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(mainCSS.content.cloneNode(true))
     this.shadowRoot.appendChild(chatTemplate.content.cloneNode(true))
-    this.connectChat()
-  }
-
-  connectChat () {
-    let chatDiv = this.shadowRoot.querySelector('#chatApp')
-    console.log(chatDiv)
-    chatDiv.addEventListener('keypress', (e) => {
+    this.chatDiv = this.shadowRoot.querySelector('#chatApp')
+    this.connectChat().then(socket => {
+      this.sendMessage()
+    })
+    this.chatDiv.addEventListener('keypress', (e) => {
       // listen for Enter Key
       if (e.keyCode === 13) {
         this.sendMessage(e.target.value)
@@ -24,12 +23,59 @@ class Chat extends window.HTMLElement {
     })
   }
 
-  sendMessage (text) {
+  connectChat () {
+    return new Promise((resolve, reject) => {
+      if (this.socket && this.socket.readyState === 1) {
+        resolve(this.socket)
+        return
+      }
 
+      this.socket = new WebSocket('ws://vhost3.lnu.se:20080/socket/')
+
+      this.socket.addEventListener('open', e => {
+        resolve(this.socket)
+      })
+
+      this.socket.addEventListener('error', e => {
+        reject(new Error('could not connect to server'))
+      })
+
+      this.socket.addEventListener('message', e => {
+        let message = JSON.parse(e.data)
+        if (message.type === 'message') {
+          this.printMessage(message)
+        }
+      })
+    })
   }
 
-  printMessage () {
+  sendMessage (text) {
+    let data = {
+      type: 'message',
+      data: text,
+      username: 'Nilsson',
+      channel: '',
+      key: this.apiKey
+    }
 
+    this.connectChat().then(socket => {
+      socket.send(JSON.stringify(data))
+    }).catch(error => {
+      console.log('something went wrog', error)
+    })
+    console.log('sending message', text)
+  }
+
+  printMessage (message) {
+    let template = this.chatDiv.querySelectorAll('template')[0]
+    console.log(template)
+
+    let messageDiv = document.importNode(template.content.firstElementChild, true)
+    console.log(messageDiv)
+    messageDiv.querySelectorAll('.text')[0].textContent = message.data
+    messageDiv.querySelectorAll('.autor')[0].textContent = message.username
+
+    this.shadowRoot.querySelectorAll('.messages')[0].appendChild(messageDiv)
   }
 }
 
